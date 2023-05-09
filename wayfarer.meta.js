3ec2031dee3d9e39e61b477976c87f93c863a164
DavidGamings <davidp@peetzon.nl>
2023-05-09 23:36:18 +0200

    Update wayfarer.meta.js

--- wayfarer.meta.js
+++ wayfarer.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        WayfarerApp
 // @namespace   example
-// @version     1.0.3
+// @version     1.0.4
 // @description Changes the background color of the webpage
 // @match       https://wayfarer.nianticlabs.com/*
 // @downloadURL https://github.com/davidgamings/wayfarer/raw/main/wayfarer.js
